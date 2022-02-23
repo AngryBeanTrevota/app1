b0e6abbe96ed23705d3081a3e3a00a62aee8d3d7
AngryBeanTrevota <58980330+AngryBeanTrevota@users.noreply.github.com>
2022-02-23 08:20:50 -0300

    Transforma todo o conteúdo no objeto "course"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const Header = (props) => {
   console.log(props)
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -22,7 +22,7 @@ const Content = (props) => {
   console.log(props)
   return(
     <div>
-      {props.parts.map(value => <Part part={value.name} exercises={value.exercises}/>)}
+      {props.course.parts.map(value => <Part part={value.name} exercises={value.exercises}/>)}
     </div>
   )
 }
@@ -30,7 +30,7 @@ const Content = (props) => {
 const Total = (props) => {
   console.log(props)
   let soma = 0
-  props.parts.forEach(element => {
+  props.course.parts.forEach(element => {
     soma = soma + element.exercises
   });
   return(
@@ -41,27 +41,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
