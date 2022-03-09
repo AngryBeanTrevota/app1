b4f2f15da6e4e81dd77e19070f32325aaf51cfd5
AngryBeanTrevota <58980330+AngryBeanTrevota@users.noreply.github.com>
2022-03-09 06:13:49 -0300

    Componente "Button" criado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+  {props.text}
+  </button>
+)
+
+const App = () => {
+  // Save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+/*
 const Header = (props) => {
   console.log(props)
   return (
@@ -67,5 +88,6 @@ const App = () => {
     </div>
   )
 }
+*/
 
 export default App
