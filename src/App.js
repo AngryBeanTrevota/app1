a0f95ed41dc5caf062bea6da1a474930a50e2374
AngryBeanTrevota <58980330+AngryBeanTrevota@users.noreply.github.com>
2022-04-05 09:22:15 -0300

    Adciona o número no input e no display depois do nome

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: '22450386'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-  const addName = (event) => {
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
     event.preventDefault()
     console.log("persons: ", persons)
     console.log("new name: ", newName)
@@ -21,9 +27,10 @@ const App = () => {
     }
     else
     {
-      const nameObject = { name: newName }
+      const nameObject = { name: newName , number: newNumber}
       setPersons(persons.concat(nameObject))
       setNewName("")
+      setNewNumber("")
     }
   }
 
@@ -36,12 +43,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit" onClick={addName}>add</button>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {
-        persons.map((person, i) => <div key={persons.length + i}>{person.name}</div> ) // Somei com o índice porque senão os dois primeiros tem o mesmo índice e dá erro
+        persons.map((person, i) => <div key={persons.length + i}>{person.name}:  {person.number}</div> ) // Somei com o índice porque senão os dois primeiros tem o mesmo índice e dá erro
       }
     </div>
   )
