32720c0ec51786c37128724428605635668139d3
AngryBeanTrevota <58980330+AngryBeanTrevota@users.noreply.github.com>
2022-04-05 09:04:04 -0300

    Esqueci de dar push no 2.6, então vai com o 2.7

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
-import React from 'react'
-import Course from './components/Course' 
-
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
-  return <Course courses={courses} />
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log("persons: ", persons)
+    console.log("new name: ", newName)
+    if(persons.find(element => element.name === newName.trim()))
+    {
+      alert(`${newName.trim()} is already added to phonebook`);
+    }
+    else
+    {
+      const nameObject = { name: newName }
+      setPersons(persons.concat(nameObject))
+      setNewName("")
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>debug: {newName}</div>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addName}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map((person, i) => <div key={persons.length + i}>{person.name}</div> ) // Somei com o índice porque senão os dois primeiros tem o mesmo índice e dá erro
+      }
+    </div>
+  )
 }
 
 export default App
