f7f3da78fef3f79eb8a995fef606cec650a12023
AngryBeanTrevota <58980330+AngryBeanTrevota@users.noreply.github.com>
2022-04-06 09:49:36 -0300

    Coloca a função de filtrar a lista

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '22450386'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterInput, setFilterInput] = useState('')
+  const [filteredList, setFilteredList] = useState(persons)
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {             // o filterInput ta diferente do target e eu nn sei pq
+    setFilterInput(event.target.value)
+    console.log("filter input:", filterInput)
+    console.log("event target:", event.target.value)
+    if(event.target.value.trim())
+    {
+      setFilteredList(persons.filter((person) => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
+    }
+    else
+    {
+      setFilteredList(persons)
+    } 
   }
 
   const addPerson = (event) => {
     event.preventDefault()
-    console.log("persons: ", persons)
-    console.log("new name: ", newName)
+    //console.log("persons: ", persons)
+    //console.log("new name: ", newName)
     if(persons.find(element => element.name === newName.trim()))
     {
       alert(`${newName.trim()} is already added to phonebook`);
@@ -40,6 +60,9 @@ const App = () => {
       <div>debug: {newName}</div>
       <form>
         <div>
+        filter shown with: <input value={filterInput} onChange={handleFilterChange}/>
+        </div>
+        <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
@@ -51,7 +74,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map((person, i) => <div key={persons.length + i}>{person.name}:  {person.number}</div> ) // Somei com o índice porque senão os dois primeiros tem o mesmo índice e dá erro
+        filteredList.map((person, i) => <div key={persons.length + i}>{person.name}:  {person.number}</div> ) // Somei com o índice porque senão os dois primeiros tem o mesmo índice e dá erro
       }
     </div>
   )
