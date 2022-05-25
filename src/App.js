783affed362878e4d87e2b7d83061445bc58efb6
AngryBeanTrevota <58980330+AngryBeanTrevota@users.noreply.github.com>
2022-05-25 06:08:05 -0300

    Pega contatos iniciais do servidor JSON

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({input, onChangeFunction}) => {
   return(
@@ -37,17 +38,19 @@ const Numbers = ({filteredList, persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterInput, setFilterInput] = useState('')
   const [filteredList, setFilteredList] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   useEffect(() => {
     if(filterInput.trim())
